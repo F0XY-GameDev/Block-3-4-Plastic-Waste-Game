257339dfbbf19f1872ef31d230e0d3ae9e645779
Nicole <nicolewiedmaier97@gmail.com>
2022-06-14 14:10:37 +0200

    all assets should be linked + cutscene narrative mildly edited

---
+++ PWAG/Assets/showOnButtonPress.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class showOnButtonPress : MonoBehaviour
+{
+    public GameObject objectToShow;
+    public float currentOpacity;
+    public bool isShowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentOpacity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectToShow.GetComponentInChildren<Image>().color = new Color(1.0f, 1.0f, 1.0f, currentOpacity);
+        if (Input.GetKeyDown("joystick button 1") && !isShowing)
+        {
+            currentOpacity = 1.0f;
+        } 
+        if (Input.GetKeyDown("joystick button 1") && isShowing)
+        {
+            currentOpacity = 0f;
+        }
+    }
+}
